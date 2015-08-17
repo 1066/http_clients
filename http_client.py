7fb19e55c881191336a4199e067ab2c494c162df
1066 <git@1066.io>
2015-08-17 14:21:56 -0400

    second commit

--- http_client.py
+++ http_client.py
@@ -3,20 +3,42 @@
 import requests
 from bs4 import BeautifulSoup
 
-# The URL you will request 
-#url = raw_input('Enter URL: ')
+methods = ['head','put', 'get']
 
+
+while True:
+    user_input = input(">")
+          
+    if user_input == 'exit':
+        break
+
+    elif user_input == '1':
+        url = input('Enter URL: ')
+        method = input('Enter the HTTP method: ')
+       if method == method in methods:
+          r = requests.method(url)
+
+
+    def http_method(url):
+
+
+	def print_response(url):
+        response = input("Enter if you like to see either headers,cookies, status codes")
+            headers = r.headers
+            response_code = r.status_code
+	            
+
+    elif user_input == 'help' or 'h':
+        print("Enter 1 to make a request for the URL:")
+        print("Exit - to exit client")
+
+     
 #HTTP Methods
-post_method = raw_input('Choose a HTTP methd you would like to use :')
-methods = ['head', 'trace', 'put', 'get' 'delete', 'options', 'connect']
-for method in methods:
-   if method in methods == post_method
+#post_method = raw_input('Choose a HTTP methd you would like to use :')
+#methods = ['head', 'trace', 'put', 'get' 'delete', 'options', 'connect']
 
 
 r = requests.get(url) 
-
-while True:
-   print 'What are you looking foward
-
 headers = r.headers
-r.txt  = r.txt 
+text  = r.txt 
+cookies = r.cookies
