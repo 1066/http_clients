60834ae759c688a4d92514c2286f8a7c6e1a597a
1066 <git@1066.io>
2015-08-17 13:18:43 -0400

    removing input

---
+++ http_client.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3
+
+import requests
+from bs4 import BeautifulSoup
+
+# The URL you will request 
+#url = raw_input('Enter URL: ')
+
+#HTTP Methods
+post_method = raw_input('Choose a HTTP methd you would like to use :')
+methods = ['head', 'trace', 'put', 'get' 'delete', 'options', 'connect']
+for method in methods:
+   if method in methods == post_method
+
+
+r = requests.get(url) 
+
+while True:
+   print 'What are you looking foward
+
+headers = r.headers
+r.txt  = r.txt 
